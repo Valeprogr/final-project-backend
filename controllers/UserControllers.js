dc4b359528b5795f5cfd01183f81706608703773
valentina vittoria <valeviit@icloud.com>
2022-12-24 14:17:13 +0100

    add  user controllers get all and create

---
+++ controllers/UserControllers.js
@@ -0,0 +1,22 @@
+import { get } from "mongoose";
+import UserServices from "../services/UserServices";
+
+class UserControllers {
+    async getAll(req, res) {
+        try {
+            const users = await UserServices.getAll();
+            res.status(200).json(users)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const user = await UserServices.create(req.body);
+            res.status(201).json(user)
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    }
+}
